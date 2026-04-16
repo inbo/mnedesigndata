a6feaf56bb9d27149e27bb94c984dbf4a8282601
florisvdh <floris.vanderhaeghe@inbo.be>
2026-04-16 11:12:35 +0200

    REP data: document fag_stratum_grts_calendar

---
+++ R/repdata_samples.R
@@ -0,0 +1,18 @@
+#' Designed fieldwork calendar
+#'
+#' The data frame `fag_stratum_grts_calendar` represents the designed sampling
+#' unit calendar – across schemes – of the most important field activity groups.
+#' It has unique combinations of `stratum`, `grts_address`, `date_interval` and
+#' `field_activity_group`.
+#'
+#' The object is derived from the scheme-specific FAG calendars (object
+#' [`scheme_moco_ps_spsubset_fag_stratum_sppost_spsamples_calendar`]) by
+#' eliminating spatiotemporal duplication and taking into account the highest
+#' required FAG frequency across schemes.
+#'
+#' @inherit modules source format
+#'
+#' @family REP objects of the design outcome
+#'
+#' @name fag_stratum_grts_calendar
+NULL
